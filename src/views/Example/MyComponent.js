060a4e6cdd1942a51e434a38287bf23e25549d01
fitthuctap28 <fitthuctap28@FIT-BDRD03>
2023-05-08 14:32:08 +0700

    event

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,30 +1,38 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-    state={
-        name : 'JJ',
-        channel:'Hoi dan IT'
+    state = {
+        name: 'JJ',
+        channel: 'Hoi dan IT'
     }
-    handleOnchangeName =(event)=>{
+    handleOnchangeName = (event) => {
         this.setState({
-            name : event.target.value
+            name: event.target.value,
+            channel:   'abc'
         });
     }
     /*
      JSX => return block
     */
+    handleClickButtun = () => {
+        alert('click me')
+    }
+
     render() {
-        let name = 'JSX';
+    
         return (
             <React.Fragment>
                 <div className="first">
-                <input value={this.state.name} type="text"
-                  onChange={(event)=> this.handleOnchangeName(event)}
-                />
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnchangeName(event)}
+                    />
                     Hello my Name is {this.state.name}!
                 </div>
                 <div className="second">
-                   My youtobe channel : {this.state.channel}
+                    My youtobe channel : {this.state.channel}
+                </div>
+                <div className="third">
+                    <button onClick={()=>this.handleClickButtun() }>Click me</button>
                 </div>
             </React.Fragment>
         )
