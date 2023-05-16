ad9970deff45c352abb841163980d95c966c0f71
fitthuctap28 <fitthuctap28@FIT-BDRD03>
2023-05-16 16:47:10 +0700

    package

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from './Example/MyComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -8,12 +11,28 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World with React
+          Todo App
         </p>
-       
-        <MyComponent/>
+
+
+        <ListTodo />
+
       </header>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
     </div>
+
   );
 }
 
